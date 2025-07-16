8dfead9b87799528928623c8be00e090fac86e8f
ChanhoKim <chanhokim9848@gmail.com>
2025-07-16 16:08:41 +0900

    JUL/16/25

---
+++ README.md
@@ -0,0 +1,8 @@
+# Social Media App
+
+##### MongoDB, Express, React, NodeJS
+##### Clerk, Arcjet, CodeRabbit
+
+
+### Project Blog
+##### https://blog.naver.com/detol3953/223935903819 - JUL/16/25
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
+import { ENV } from "./config/env.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is running on 5001"));
-    
+app.listen(ENV.PORT, () => console.log("Server is running on 5001", ENV.PORT));
