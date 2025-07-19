0dee563895cd17b90b72beded00f2b06acdf1079
ChanhoKim <chanhokim9848@gmail.com>
2025-07-19 21:15:27 +0900

    fixed readme and server.js files

--- README.md
+++ README.md
@@ -17,3 +17,5 @@
 ##### [User routes - JUL/17/25](https://blog.naver.com/detol3953/223937058607)
 
 ##### [Post routes functionality - JUL/18/25](https://blog.naver.com/detol3953/223938071338)
+
+##### [comment functions - JUL/19/25](https://blog.naver.com/detol3953/223939778005)
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
@@ -23,6 +24,8 @@ app.get("/", (req, res) => res.send("Hello from server"));
 app.use("/api/users", userRoutes);
 // post route api
 app.use("/api/posts", postRoutes);
+// comments route api
+app.use("/api/comments", commentRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
