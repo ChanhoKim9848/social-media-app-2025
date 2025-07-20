84ebdf2160bf0d7e5dfb839eb82b06d2438be984
ChanhoKim <chanhokim9848@gmail.com>
2025-07-20 12:28:36 +0900

    fixed post.route.js

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -7,7 +7,8 @@ import {
   getUserPosts,
   likePost,
 } from "../controllers/post.controller.js";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
